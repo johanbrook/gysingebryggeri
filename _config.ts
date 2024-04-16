d1edfb2633bb6357b90f91a1eef3660bf3f23eaf
Johan Brook <johan@lookback.io>
2024-04-16 09:35:58 +0200

    Fix basepath when temporarily beneath a github.io URL

--- _config.ts
+++ _config.ts
@@ -3,17 +3,20 @@ import date from "lume/plugins/date.ts";
 import postcss from "lume/plugins/postcss.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
 import og_images from "lume/plugins/og_images.ts";
+import basePath from "lume/plugins/base_path.ts";
 import postcssUtopia from "npm:postcss-utopia@^1";
 
 const site = lume({
     src: "./src",
     dest: "./build",
+    location: new URL("https://johanbrook.github.io/gysingebryggeri"), // ignored in dev
 });
 
 site
     //
     .copy("public", ".")
     .use(date())
+    .use(basePath())
     .use(
         postcss({
             includes: "_css",
